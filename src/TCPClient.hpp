ca6e84dbcc5db4d265d9a4e1eab74d288a52d9f8
YCRiver3038 <yuuki.cityriver@gmail.com>
2025-12-23 10:04:33 +0900

    ネットワーク周りのヘッダで共通する部分を統合

--- src/TCPClient.hpp
+++ src/TCPClient.hpp
@@ -1,53 +1,7 @@
 #ifndef TCPCLIENT_H_INCLUDED
 #define TCPCLIENT_H_INCLUDED
 
-
-#include <string>
-#include <map>
-#include "stdio.h"
-#include "string.h"
-#include "stdint.h"
-
-#include "errno.h"
-
-#if defined(_WIN32) || defined(_WIN64)
-//ref https://learn.microsoft.com/ja-jp/windows/win32/api/winsock/nf-winsock-wsastartup
-//
-//    WORD wVersionRequested;
-//    WSADATA wsaData;
-//    int err;
-//
-///* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
-//    wVersionRequested = MAKEWORD(2, 2);
-
-//    err = WSAStartup(wVersionRequested, &wsaData);
-//    if (err != 0) {
-//        /* Tell the user that we could not find a usable */
-//        /* Winsock DLL.                                  */
-//        printf("WSAStartup failed with error: %d\n", err);
-//        return 1;
-//    }
-
-#include <winsock2.h>
-#include "winsock.h"
-#include <windows.h>
-#include <ws2tcpip.h>
-
-// Need to link with Ws2_32.lib
-#pragma comment(lib, "ws2_32.lib")
-
-#define close(close_fd) closesocket(close_fd)
-#else
-#include <sys/select.h>
-#include "fcntl.h"
-#include "unistd.h"
-#include "netdb.h"
-#include "sys/poll.h"
-#include "sys/types.h"
-#include "arpa/inet.h"
-#include "netinet/ip.h"
-#include <sys/socket.h>
-#endif
+#include "NWCommon.hpp"
 
 #define TCLI_TIMEOUT_MSEC 1000
 #define TCLI_TIMEOUT_SEC 1
